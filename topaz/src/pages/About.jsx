c61d28f3f38f626438e8777f9067f6929162ea9e
Funsho <oluwafunshoalabi97@gmail.com>
2026-03-12 14:19:00 +0100

    api

---
+++ topaz/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='contain-center'>
+        <div>
+            <h1>About</h1>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- topaz/src/pages/ExpertOption.jsx
+++ topaz/src/pages/ExpertOption.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MobileNav from '../components/MobileNav'
 
 function ExpertOption() {
-    const [text, setText] = useState("Hello");
+    const [datas, setData] = useState([])
 
-
-    const handleStart = ()=>{
-        setText("Bot Started")
+    
+    const getSignals = async () => {
+    const res = await fetch("http://localhost:8000/signals")
+    const mData = await res.json()
+    setData(mData)
+    console.log(mData)
     }
+
+
+    // const handleStart = ()=>{
+    //     getSignals();
+    //     console.log(data)
+    // }
     const handleStatus = ()=>{
         setText("Checking bot status")
     }
@@ -19,9 +28,25 @@ function ExpertOption() {
   return (
     <div className='expert-display white-bg-2'>
         <MobileNav />
-        <div className='signal-display'>{text}</div>
+        <div className='signal-display'>
+            {datas.map(data => (
+                <div key={data.id}>
+                    <p>{data.pair}</p>
+                    <p>{data.price}</p>
+                    <p>{data.signal}</p>
+                    <p>{data.strength}</p>
+                    <p>{data.timeFrame}</p>
+                    <p>{data.entrytime}</p>
+                </div>
+            ))}
+            <div className='contain-justify-between'>
+                <div></div>
+                <div>time</div>
+            </div>
+            <hr />
+        </div>
         <div className='action-btn'>
-            <button onClick={handleStart}>Start</button>
+            <button onClick={getSignals}>Start</button>
             <button onClick={handleStatus}>Status</button>
             <button onClick={handleStop}>Stop</button>
         </div>
