f07997f5986b9b5996acc5a8dbef009a6700167e
Funsho <oluwafunshoalabi97@gmail.com>
2026-03-13 13:58:43 +0100

    # api

--- topaz/src/pages/ExpertOption.jsx
+++ topaz/src/pages/ExpertOption.jsx
@@ -1,57 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import MobileNav from '../components/MobileNav'
+import React, { useEffect, useState } from "react";
+import MobileNav from "../components/MobileNav";
 
 function ExpertOption() {
-    const [datas, setData] = useState([])
+  const [signal, setSignal] = useState([]);
 
-    
-    const getSignals = async () => {
-    const res = await fetch("http://localhost:8000/signals")
-    const mData = await res.json()
-    setData(mData)
-    console.log(mData)
-    }
+  const getSignals = async () => {
+    const res = await fetch("https://topaz-backend-vvuz.onrender.com/signals");
+    const data = await res.json();
+    setSignal(data);
+    console.log(data);
+  };
 
-
-    // const handleStart = ()=>{
-    //     getSignals();
-    //     console.log(data)
-    // }
-    const handleStatus = ()=>{
-        setText("Checking bot status")
-    }
-    const handleStop = ()=>{
-        setText("Bot Stopped")
-    }
-
+  // const handleStart = ()=>{
+  //     getSignals();
+  //     console.log(data)
+  // }
+  const handleStatus = () => {
+    setText("Checking bot status");
+  };
+  const handleStop = () => {
+    setText("Bot Stopped");
+  };
 
   return (
-    <div className='expert-display white-bg-2'>
-        <MobileNav />
-        <div className='signal-display'>
-            {datas.map(data => (
-                <div key={data.id}>
-                    <p>{data.pair}</p>
-                    <p>{data.price}</p>
-                    <p>{data.signal}</p>
-                    <p>{data.strength}</p>
-                    <p>{data.timeFrame}</p>
-                    <p>{data.entrytime}</p>
-                </div>
-            ))}
-            <div className='contain-justify-between'>
-                <div></div>
-                <div>time</div>
-            </div>
-            <hr />
+    <div className="expert-display white-bg-2">
+      <MobileNav />
+      <div className="signal-display">
+        {signal.map((sign) => (
+          <div key={sign.id} className="signal-card">
+            <h3>{sign.pair}</h3>
+            <p>Price: {sign.price}</p>
+            <p>Signal: {sign.signal}</p>
+            <p>Strength: {sign.strength}</p>
+            <p>Timeframe: {sign.timeFrame}</p>
+            <p>Entry Time: {sign.entrytime}</p>
+          </div>
+        ))}
+        <div className="contain-justify-between">
+          <div></div>
+          <div>time</div>
         </div>
-        <div className='action-btn'>
-            <button onClick={getSignals}>Start</button>
-            <button onClick={handleStatus}>Status</button>
-            <button onClick={handleStop}>Stop</button>
-        </div>
+        <hr />
+      </div>
+      <div className="action-btn">
+        <button onClick={getSignals}>Start</button>
+        <button onClick={handleStatus}>Status</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ExpertOption
+export default ExpertOption;
